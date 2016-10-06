7defdf6d49de37586f44963604870c0cbcddf138
Tiansi Dong <tdong@uni-bonn.de>
2016-10-06 11:42:02 +0200

    alpha version of xml2fdp

--- os_fdp_adapters/xml-adapter/xml_to_fdp.py
+++ os_fdp_adapters/xml-adapter/xml_to_fdp.py
@@ -1,121 +1,86 @@
-import json
-import subprocess
-import threading
-import os
 import sys
-from time import sleep
+import csv
+import json
+import tempfile
+import os
+import six
 
-#github lib of the xml->csv package
-XCGITHUB="https://github.com/wk0206/testPorject2.git"
-#nodejs package name of the xml->csv
-XCPACKAGE = "combineXMLdata"
-PORT = 8888
-TARGET_BASE_PATH = '/tmp/transformTool'
-
-testfile1 = "https://raw.githubusercontent.com/wk0206/testPorject2/master/test_simple.xml" #works
-testfile2 = "file:///Users/tdong/git/os-fdp-adapters/os_fdp_adapters/xml-adapter/tests/test_eu2014_10.xml" #not work for local file for the moment
-testfile3 = "http://www.wenxion.net/pub/test.xml" #works
-
-toolList = ['npm', 'node', 'curl']
+from common.fetchers import fetch_local_filename
+from common.urls import wrap
 
 
-def check_tools():
-    for tl in toolList:
-        checkFail = subprocess.call(['which', tl])
-        if checkFail:
-            print('Missing tools: ', tl)
-            return False
-    return True
+def to_bytes(x):
+    if six.PY2 and type(x) is six.text_type:
+        return x.encode('utf8')
+    return x
 
 
-def no_xml2csv_package():
-    for ftuple in os.walk('.'):
-        if XCPACKAGE in ftuple[0]:
-            return False
-    return True
-
-
-def is_service_started(PORT):
-    if subprocess.call(["lsof", "-i:"+str(PORT)]) == 0:
-        return True
-    else:
-        return False
-
-
-def get_process_id_str_using(PORT):
-    out = subprocess.check_output(["lsof", "-i:"+str(PORT)])
-    return out.split()[10].decode("utf-8") 
-    
-
-def xml2csv(xmlLink):
-    if (no_xml2csv_package() and check_tools()):
-        subprocess.call(["npm", "install", XCGITHUB])
-        if is_service_started(PORT):
-            pid_str = get_process_id_str_using(PORT)
-            subprocess.call(['kill', '-9', pid_str])
-    elif (no_xml2csv_package() and  not check_tools()):
-        print('tools missing for installing xml2csv package.\n')
-        return 
-    
-    if not is_service_started(PORT):
-        subprocess.Popen(['node', 'node_modules/'+XCPACKAGE, '&'])  
-        print('service started...')
-        sleep(5)
-        xmlLink = "http://localhost:8888/\[{}\]".format(xmlLink)
-    else:
-        xmlLink = "http://localhost:8888/\[{}\]".format(xmlLink)
-    
-    command = "curl \'"+xmlLink + "' &"
-    os.system(command)
-
-
-def generate_fdp(xmlLink):
+def gen_csv_file(url):
     """
-    :param xmlLink: https://XXXXX/test_simple_obeu.xml
-    :return: print the datapackage.json on the stdout
-
+    :param url: url can be the absolute address of an xml file, or  an xml file located at web (http://..../<name>.xml
+    :return: list of csv files, each contains a data-table extracted from the input xml file
     """
-    global TARGET_BASE_PATH
-    basename = os.path.splitext(os.path.basename(xmlLink))[0]
-    targetpath = os.path.join(TARGET_BASE_PATH, basename)
-    sourcelst = []
+    orginBaseName = os.path.basename(url).strip(' ')
+    with tempfile.NamedTemporaryFile(delete=False) as fd:
+        cmd = "xml2csv {} -a 135 >> {}".format(url, fd.name)
+        print('running ', cmd)
+        os.system(cmd)
+        return split_into_single_csvs(fd.name, orginBaseName)
 
-    while not os.path.isdir(targetpath):
-        print('waiting...for... producing.. in..', targetpath)
-        sleep(3)
 
-    found = False
-    wait_time = 60
-    count = 0
-    while not found:
-        for file in os.listdir(targetpath):
-            if os.path.isdir(os.path.join(targetpath, file)):
-                total_csvs = int(file)
-                for i in range(total_csvs):
-                    csvfile = str(i)+".csv"
-                    sourcelst.append({'path': os.path.join(targetpath, csvfile)})
-                found = True
-        sleep(3)
-        count += 4
-        if count > wait_time:
-            break
-    print()
+def split_into_single_csvs(filename, orginBaseName, tableSeparator="\n\n\n"):
+    """
+    :param  filename: a file containing several data-tables, which are separated by blank lines
+    :return: a list of csv file names, each is csv file containing one data-table
+    """
+    with open(filename, 'r') as fd:
+        data = fd.read().split("file exist\n")[-1]
+        count = -1
+        csvLst = []
+        for oneTable in data.split(tableSeparator):
+            if oneTable != '':
+                count += 1
+                tmpFile = ""
+                with tempfile.NamedTemporaryFile(delete=False) as csvFd:
+                    csvFd.write(str.encode(oneTable, 'utf8'))
+                    tmpFile = csvFd.name
+                path, base = os.path.split(tmpFile)
+                csvFileName = os.path.join(path, orginBaseName+"_"+str(count)+"_"+base+".csv")
+                os.renames(tmpFile , csvFileName)
+                csvLst.append(csvFileName)
+    return csvLst
+
+
+filename = sys.argv[1]
+if filename.endswith('.csv'):
+    filename = fetch_local_filename(filename)
+    data = []
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        data = [[to_bytes(cell) for cell in row] for row in csvreader]
+        
+    w = csv.writer(sys.stdout)
+    w.writerows(data)
+
+else:
+    """
+    input url link of xml,
+    extract csv files, and save them as template files
+    wrap file names, and list them in the 'resources' key
+    """
+    csvLst = gen_csv_file(filename)
+    nameLst = []
+    for csvname in csvLst:
+        nameLst.append({"path": wrap(csvname)})
+
     json.dump({
-        "name": basename,
-        "title": basename,
+        "name": "xml2csv",
+        "title": "xml2csv",
         "model": {
-            "measures": {},
-            "dimensions": {}
+            "measures":{},
+            "dimensions":{}
         },
-        "resources": sourcelst
+        "resources": [
+            nameLst
+        ]
     }, sys.stdout, sort_keys=True)
-    print()
-
-
-def xml_2_fdp(xmlLink):
-    xml2csv(xmlLink)
-    generate_fdp(xmlLink)
-
-
-xmlAtWeb = sys.argv[1]
-xml_2_fdp(xmlAtWeb)
