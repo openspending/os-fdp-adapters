701d87ad8996c40c3752d18c407450c59ba1010d
Brook Elgie <specialbrew@gmail.com>
2017-11-24 13:51:16 +0000

    Add tox for local testing

--- setup.py
+++ setup.py
@@ -1,20 +1,48 @@
+import os
+import io
+from setuptools import setup, find_packages
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+__version__ = '0.0.1'
 
-config = {
-    'description': 'OS FDP Adapters',
-    'author': 'Adam Kariv',
-    'url': 'http://github.com/openspending/os-fdp-adapters',
-    'download_url': 'http://github.com/openspending/os-fdp-adapters',
-    'author_email': 'adam.kariv@okfn.org',
-    'version': '0.1',
-    'install_requires': [],
-    'packages': ['os_fdp_adapters'],
-    'scripts': [],
-    'name': 'projectname'
-}
 
-setup(**config)
+# Helpers
+def read(*segments):
+    path = os.path.join(*segments)
+    with io.open(path, encoding='utf-8') as f:
+        return f.read().strip()
+
+
+README = read('README.md')
+
+setup(
+    name='os_fdp_adapters',
+    version=__version__,
+    description='Convert various data formats into FDP',
+    long_description=README,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='api fiscal datapackage fdp openspending',
+    author='OpenSpending',
+    author_email='info@openspending.org',
+    url='http://github.com/openspending/os-fdp-adapters',
+    download_url='http://github.com/openspending/os-fdp-adapters',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    package_data={
+        '': ['*.json'],
+    },
+    zip_safe=False,
+    install_requires=[
+        # We're using requirements.txt
+    ],
+    tests_require=[
+        'tox',
+    ]
+)
